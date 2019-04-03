0f15cacc53b84d77d3b5a3daaeaa407f9ed5cbc5
Kings Munene <gmaxkings@gmail.com>
2019-04-04 00:07:53 +0300

    add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router';
+//Defining routes
+const routes:Routes=[
+  {path:"landingpage",component:LandingpageComponent},
+  {path:"about",component:AboutComponent}
+]
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage/landingpage.component';
 
-import {UserService} from './service/user.service'
+import {UserService} from './service/user.service';
+import { AboutComponent } from './about/about.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
