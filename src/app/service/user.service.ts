969582ba7edb86b02864b006d52e41b07887c5fb
Kings Munene <gmaxkings@gmail.com>
2019-04-03 22:40:28 +0300

    add changes in import

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { User } from '../user';
+import {HttpClient} from '@angular/common/http'
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
+
+
 export class UserService {
+  userLink: string = 'https://api.github.com/users/';
+  userName:string;
 
-  constructor() { }
+constructor(private http:HttpClient){
+  console.log("service is now ready!");
+  this.userName="KingsMunene"
+}
+getUserInformation(){
+  return this.http.get(this.userLink + this.userName)
+}
+getUserRepos(){
+  return this.http.get(this.userLink + this.userName + "/repos")
+}
+updateUser(userName:string){
+  this.userName=userName;
+}
 }
