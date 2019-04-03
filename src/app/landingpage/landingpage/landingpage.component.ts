c78312bd115ef6220556760fa107d241e4e2f7cd
Kings Munene <gmaxkings@gmail.com>
2019-04-03 22:42:15 +0300

    add function getUserRepositories

--- src/app/landingpage/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage/landingpage.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import {UserService } from '../../service/user.service';
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
   styleUrls: ['./landingpage.component.css']
 })
 export class LandingpageComponent implements OnInit {
+ user:any;
+ repos:any;
+ userName:string;
 
-  constructor() { }
+  constructor(private userservice:UserService) { 
+   
+  }
+  findUser(){
+    this.userservice.updateUser(this.userName);
+    this.userservice.getUserInformation().subscribe(user => {
+      console.log(user);
+      this.user= user;
+    });
+    this.userservice.getUserRepos().subscribe(repos =>{
+      console.log(repos);
+      this.repos=repos;
+    })
+      
+  }
+  ngOnInit(){
 
-  ngOnInit() {
+  }
   }
 
-}
+
